95401efd8083c8f7a75a85c37d003dc5b97910a5
Suresh Krishna Tavva <sureshkrishnat@inforaise.com>
2025-03-18 15:57:08 +0530

    Installed packages

---
+++ src/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace smart_local
+{
+    class program
+    {
+        static void Main(String[] args)
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
